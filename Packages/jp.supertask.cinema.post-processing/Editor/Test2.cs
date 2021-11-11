82b84b078c68184d9d7cf1e05628f24d7e6542ae
Tasuku TAKAHASHI <lightfox.task@gmail.com>
2021-11-11 22:33:59 +0900

    Fixed conflicted dir.

--- Packages/jp.supertask.cinema.post-processing/Editor/Test2.cs
+++ Packages/jp.supertask.cinema.post-processing/Editor/Test2.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Test : MonoBehaviour
+public class Test2 : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
